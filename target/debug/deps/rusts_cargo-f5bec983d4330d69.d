0cdca887c9b0330289674296b16db6868d83e0a5
keisugano <tobasojyo@gmail.com>
2018-03-30 21:04:34 +0900

    cargo package built

---
+++ target/debug/deps/rusts_cargo-f5bec983d4330d69.d
@@ -0,0 +1,5 @@
+/Users/keisugano/rusts/target/debug/deps/rusts_cargo-f5bec983d4330d69: src/main.rs
+
+/Users/keisugano/rusts/target/debug/deps/rusts_cargo-f5bec983d4330d69.d: src/main.rs
+
+src/main.rs:
---
+++ target/debug/rusts_cargo.d
@@ -0,0 +1,1 @@
+/Users/keisugano/rusts/target/debug/rusts_cargo.dSYM: /Users/keisugano/rusts/src/main.rs
