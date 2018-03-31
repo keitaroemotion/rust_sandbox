a8c48918232811f1a1ebd19e13d5dd3dbb9a1ba1
keisugano <tobasojyo@gmail.com>
2018-03-31 11:35:20 +0900

    main.rs updated: guessing

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,13 @@
+use std::io;
+
 fn main() {
-    println!("Hello, worldmo!");
+    println!("guess the number");
+    println!("Please input your guess.");
+    // mut means mutable, since by default in Rust
+    // variables are mutable.
+    let mut guess = String::new();
+    io::stdin()
+        .read_line(&mut guess)
+        .expect("Failed to read line");
+    println!("You guessed {}", guess);
 }
