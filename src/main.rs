74e41530073c3f7138877b3dbf3fa72c4486a118
keisugano <tobasojyo@gmail.com>
2018-03-31 12:08:59 +0900

    random number generated

--- src/main.rs
+++ src/main.rs
@@ -1,7 +1,15 @@
+extern crate rand;
+
 use std::io;
+use rand::Rng;
 
 fn main() {
     println!("guess the number");
+
+    let secret_number = rand::thread_rng().gen_range(1, 101);
+
+    println!("The secret number is: {}", secret_number);
+
     println!("Please input your guess.");
     // mut means mutable, since by default in Rust
     // variables are mutable.
